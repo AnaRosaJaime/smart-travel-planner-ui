3ea9ff3f8b3feb8531468d38d0ed6f1bd44dfcc7
Ana Jaime Perez <anajaimeperez@Anas-MacBook-Pro.local>
2025-09-04 17:40:49 +0200

    First commit

---
+++ app/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import AddToCart from "../AddToCard";
+// import styles from "./ProductCard.module.css";
+
+const ProductCard = () => {
+  return <AddToCart />;
+};
+
+export default ProductCard;
